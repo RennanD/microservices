f1c99175d3dc5f93e4626b22cc0bd4057b2ad05c
RennanD <rplaykl@gmail.com>
2021-10-10 11:14:15 -0300

    chore: init backoffice

---
+++ backoffice/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import { routes } from './routes';
+
+const app = express();
+
+app.use(routes);
+
+app.listen(process.env.PORT || 3333, () => {
+  console.log('Server running 🚀');
+});
