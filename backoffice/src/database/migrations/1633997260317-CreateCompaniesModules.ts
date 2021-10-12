848b1a8aba8e417371730d2a172f6c03585f9714
RennanD <rplaykl@gmail.com>
2021-10-11 21:27:23 -0300

    chore: create company and module relationship on db

---
+++ backoffice/src/database/migrations/1633997260317-CreateCompaniesModules.ts
@@ -0,0 +1,76 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateCompaniesModules1633997260317 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'companies_modules',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+          },
+          {
+            name: 'company_id',
+            type: 'varchar',
+          },
+          {
+            name: 'module_id',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'companies_modules',
+      new TableForeignKey({
+        name: 'FKCompanyModule',
+        referencedTableName: 'companies',
+        referencedColumnNames: ['id'],
+        columnNames: ['company_id'],
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'companies_modules',
+      new TableForeignKey({
+        name: 'FKModuleCompany',
+        referencedTableName: 'modules',
+        referencedColumnNames: ['id'],
+        columnNames: ['module_id'],
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('companies_modules', 'company_id');
+    await queryRunner.dropForeignKey('companies_modules', 'module_id');
+    await queryRunner.dropTable('companies_modules');
+  }
+}
