719e4bf1b79cb61f6bc8086879a610c1de30d435
RennanD <rennan.douglas@objetivopi.com.br>
2021-10-12 14:42:49 -0300

    chore: move docker-compose to root

--- backoffice/ormconfig.js
+++ backoffice/ormconfig.js
@@ -1,14 +1,14 @@
 module.exports = {
-  name: "default",
-  type: "postgres",
-  host: "localhost",
+  name: 'default',
+  type: 'postgres',
+  host: 'localhost',
   port: 5432,
-  username: "docker",
-  password: "docker",
-  database: "backoffice_db",
-  migrations: ["./src/database/migrations/*.ts"],
-  entities: ["./src/entities/*.ts"],
+  username: 'docker',
+  password: 'docker',
+  database: 'backoffice_db',
+  migrations: ['./src/database/migrations/*.ts'],
+  entities: ['./src/entities/*.ts'],
   cli: {
-    migrationsDir: "./src/database/migrations",
-  }
-}
+    migrationsDir: './src/database/migrations',
+  },
+};
